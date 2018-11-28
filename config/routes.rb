7e7f1f14fb77d77f5b0c63a819ec167ed4f9501e
Jose Degollado <josedegollado@MacBook-Air-de-Jose.local>
2018-11-27 21:54:37 -0600

    added devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :blogs
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+
+  root to: 'pages#home'
+
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

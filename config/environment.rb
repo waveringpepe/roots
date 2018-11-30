4929529e2a1d3d7822e070a88d42e619b9488295
Jose Degollado <josedegollado@MacBook-Air-de-Jose.local>
2018-11-30 13:18:32 -0600

    hide sendgridkeys

--- config/environment.rb
+++ config/environment.rb
@@ -9,8 +9,8 @@ ActionMailer::Base.smtp_settings = {
 :address => 'smtp.sendgrid.net',
 :port => 587,
 :authentication => :plain,
-:user_name => 'apikey',
-:password => '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',
+:user_name => ENV["SENDGRID_USERNAME"],
+:password => ENV["SENDGRID_PASSWORD"],
 :domain => 'heroku.com',
 :enable_starttls_auto => true
 

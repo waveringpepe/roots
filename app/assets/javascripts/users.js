5d08b7746815830a58eb98990e6474039036ad2a
Jose Degollado <josedegollado@MacBook-Air-de-Jose.local>
2018-12-05 13:49:51 -0600

    Added Stripe response handler and errors handling

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -12,6 +12,7 @@ $(document).on('turbolinks:load',function(){
 
 	//Prevent default submission behavior
 	event.preventDefault();
+	submitBtn.val("Proccesing").prop("disabled,true");
 
 	//Collect the credit card fields.
 	var ccNum = $('#card_number').val(),
@@ -19,16 +20,55 @@ $(document).on('turbolinks:load',function(){
 		expMonth = $('#card_month').val(),
 		expYear = $('#card_year').val();
 
-	//Send the card information to Stripe.
-	Stripe.createToken({
-	  number: ccNum,
-	  cvc: cvcNum,
-	  exp_month: expMonth,
-	  exp_year: expYear
-	}, stripeResponseHandler);
-	
+	//User Stripe JS library to check for card errors.
+	var error = false;
+
+	//Validate card number.
+	if(!Stripe.card.validateCardNumber(ccNum)) {
+		error = true;
+		alert('The credit card number appears to be invalid');
+	}
+
+	//Validate CVC number.
+	if(!Stripe.card.validateCVC(cvcNum)) {
+		error = true;
+		alert('The CVC number appears to be invalid');
+	}
+
+	//Validate expiration date.
+	if(!Stripe.card.validateExpiry(expMonth, expYear)) {
+		error = true;
+		alert('The expiration date appears to be invalid');
+	}
+
+	if (error) {
+		//If there are card errors, don't send to Stripe.
+		submitBtn.prop('disabled',false).val("Sign Up");
+	} else {
+		//Send the card information to Stripe.
+		Stripe.createToken({
+			number: ccNum,
+			cvc: cvcNum,
+			exp_month: expMonth,
+			exp_year: expYear
+		}, stripeResponseHandler);
+    }
+
+	return false;
+
 	});	
+
 	//Stripe will return back the card token.
-	//Inject card token as hidden field into form
-	//Subtmit form to our Rails app
+	function stripeResponseHandler(status, response) {
+
+		//Get the token from the response.
+		var token = response.id;
+
+		//Inject card token as hidden field.
+		theForm.append( $('<input type="hidden" name="user[stripe_card_token]">').val(token));
+		
+		//Subtmit form to our Rails app
+		theForm.get(0).submit();
+	}
+
 });

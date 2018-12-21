68d62ae37cb3847839354798151c2e15f020780e
Jose Degollado <josedegollado@MacBook-Air-de-Jose.local>
2018-12-21 12:38:16 -0600

    added country gem and country and city fields in the basic form

--- Gemfile
+++ Gemfile
@@ -61,6 +61,7 @@ gem 'cocoon', '~> 1.2', '>= 1.2.12'
 gem 'hirb', '~> 0.7.3'
 # Use Stripe for payment processing
 gem 'stripe', '1.48.0'
-
+# Use contry select to provide a list of countries
+gem 'country_select', '~> 3.1', '>= 3.1.1'
 # Use Figaro for ENV variables
 gem 'figaro', '1.1.1'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_05_070525) do
+ActiveRecord::Schema.define(version: 2018_12_21_180311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,19 @@ ActiveRecord::Schema.define(version: 2018_12_05_070525) do
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "subscriptions", id: :serial, force: :cascade do |t|
+    t.integer "owner_id"
+    t.string "name", null: false
+    t.string "processor", null: false
+    t.string "processor_id", null: false
+    t.string "processor_plan", null: false
+    t.integer "quantity", default: 1, null: false
+    t.datetime "trial_ends_at"
+    t.datetime "ends_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|
@@ -57,6 +70,14 @@ ActiveRecord::Schema.define(version: 2018_12_05_070525) do
     t.datetime "confirmation_sent_at"
     t.integer "plan_id"
     t.string "stripe_customer_token"
+    t.string "processor"
+    t.string "processor_id"
+    t.string "card_token"
+    t.string "card_brand"
+    t.string "card_last4"
+    t.string "card_exp_month"
+    t.string "card_exp_year"
+    t.string "country"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true

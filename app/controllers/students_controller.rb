ae10f536841c861321022bcc8da4d80cbc0dc3a2
Jose Degollado <josedegollado@MacBook-Air-de-Jose.local>
2019-01-14 12:10:23 -0600

    Added style to students and teachers homepage, fixed some navbar bugs

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,7 +3,7 @@ class StudentsController < ApplicationController
 access all: [:home ], teacher: {except: [:home]}, site_admin: :all, message: "you shall not pass"
 
 	def home
-		
+	@user = User.all
 	end
 
 end

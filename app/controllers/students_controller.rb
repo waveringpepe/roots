1d048a5ae9f0fd649ad3b59c88fd15ccbb37e876
Jose Degollado <josedegollado@MacBook-Air-de-Jose.local>
2019-01-15 17:19:40 -0600

    forbid users to see teachers and students pages before login in

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-
+before_action :authenticate_user!
 access user: [:home], teacher: {except: [:home]}, site_admin: :all, message: "you shall not pass"
 
 	def home
--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,5 +1,5 @@
 class TeachersController < ApplicationController
-
+before_action :authenticate_user!
 access teacher: [:home], user: {except: [:home]}, site_admin: :all, message: "you shall not pass"
 
 	def home

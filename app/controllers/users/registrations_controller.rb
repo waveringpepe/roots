83ba193195a68101b13a411d986d20c368d18f18
Jose Degollado <josedegollado@MacBook-Air-de-Jose.local>
2019-02-26 13:46:06 -0600

    added some translations

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,13 +10,7 @@ before_action :select_plan, only: :new
     super do |resource|
       if params[:plan]
         resource.plan_id = params[:plan]
-        if resource.plan_id == 5
-          resource.save_with_subscription
-        elsif resource.plan_id == 6
-          resource.save_with_subscription
-        elsif resource.plan_id == 7
-          resource.save_with_subscription
-        elsif resource.plan_id == 8
+        if resource.plan_id == 8
           resource.save_with_subscription            
         else
           resource.save_as_teacher
@@ -31,7 +25,7 @@ end
 
 private
   def select_plan
-    unless (params[:plan] == '1' || params[:plan] == '5'|| params[:plan] == '6'|| params[:plan] == '7' || params[:plan] == '8')
+    unless (params[:plan] == '1' || params[:plan] == '8')
       flash[:notice] = "Please select a membership plan to sign up."
       redirect_to root_url
     end

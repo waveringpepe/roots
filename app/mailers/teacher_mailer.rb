0440154d00c4f6167f7e195517bb8eb215de8726
Jose Degollado <josedegollado@MacBook-Air-de-Jose.local>
2019-04-12 14:59:36 -0500

    modify mails fot matches

--- app/mailers/teacher_mailer.rb
+++ app/mailers/teacher_mailer.rb
@@ -4,6 +4,8 @@ class TeacherMailer < ApplicationMailer
 	def match_email
 		@teacher = User.find_by(id: "#{Match.last.user_id}")
 		@student = User.find_by(id: "#{Match.last.student_id}")
+		@language = Language.find_by(id: "#{Match.last.language_id}")
+		
 
 		mail to: User.find_by(id: "#{Match.last.user_id}").email , subject: "¡#{User.find_by(id: "#{Match.last.user_id}").name} te hemos asignado a un estudiante!"
 	end

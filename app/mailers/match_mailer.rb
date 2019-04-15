db17902325c3af834d7e811c107897d54c4e5b00
Jose Degollado <josedegollado@MacBook-Air-de-Jose.local>
2019-04-15 13:33:11 -0500

    added name of user to sender

--- app/mailers/match_mailer.rb
+++ app/mailers/match_mailer.rb
@@ -6,18 +6,21 @@ class MatchMailer < ApplicationMailer
 		@teacher = User.find_by(id: "#{Match.last.user_id}")
 		@student = User.find_by(id: "#{Match.last.student_id}")
 		@language = Language.find_by(id: "#{Match.last.language_id}")
+		email_with_name = %("#{User.find_by(id: "#{Match.last.user_id}").name}" <#{User.find_by(id: "#{Match.last.user_id}").email}>) 
 
 
-		mail to: User.find_by(id: "#{Match.last.user_id}").email , subject: "¡#{User.find_by(id: "#{Match.last.user_id}").name} te hemos asignado a un estudiante!"
+
+		mail to: email_with_name, subject: "¡#{User.find_by(id: "#{Match.last.user_id}").name} te hemos asignado a un estudiante!"
 	end
 
 	def student_email
 		@teacher = User.find_by(id: "#{Match.last.user_id}")
 		@student = User.find_by(id: "#{Match.last.student_id}")
 		@language = Language.find_by(id: "#{Match.last.language_id}")
+		email_with_name = %("#{User.find_by(id: "#{Match.last.student_id}").name}" <#{User.find_by(id: "#{Match.last.student_id}").email}>)
 
 
-		mail to: User.find_by(id: "#{Match.last.student_id}").email , subject: "¡#{User.find_by(id: "#{Match.last.student_id}").name} te hemos asignado a un profesor!"
+		mail to: email_with_name, subject: "¡#{User.find_by(id: "#{Match.last.student_id}").name} te hemos asignado a un profesor!"
 	end
 
 

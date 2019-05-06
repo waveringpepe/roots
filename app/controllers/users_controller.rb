6f42c65dc7215696b44c7720fc0892947f567800
Jose Degollado <josedegollado@MacBook-Air-de-Jose.local>
2019-05-06 12:03:29 -0500

    added search filter to users index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   # GET /User
   def index
     @users = User.all
-    @users = User.order(sort_column + " " + sort_direction).paginate(:page => params[:page], :per_page => 100)
+    @users = User.paginate(:page => params[:page], :per_page => 100).order('created_at DESC')
     @renderer = custom_paginate_renderer
   end
  

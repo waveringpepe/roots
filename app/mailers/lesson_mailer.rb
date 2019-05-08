35907081b7534511f349821a1179feb143d67da2
Jose Degollado <josedegollado@MacBook-Air-de-Jose.local>
2019-05-08 13:11:47 -0500

    modify mailer to for lessons to have dates in every timezone of each user

--- app/mailers/lesson_mailer.rb
+++ app/mailers/lesson_mailer.rb
@@ -6,9 +6,10 @@ class LessonMailer < ApplicationMailer
 	def teacher_email(user)
 		@user = user
 		@student_name = User.find_by(id: "#{Lesson.last.student_id}").name
-		@lesson_date = Lesson.last.date_id.to_formatted_s(:long)
+		@lesson_date = Lesson.last.date_id.in_time_zone(User.find_by(id: "#{Lesson.last.user_id}").time_zone).to_formatted_s(:long_ordinal)
 		@lesson_duration = Lesson.last.time_duration_id/60
 		@lesson_language = Language.find_by(id: "#{Lesson.last.language_id}").name
+		@teacher_timezone = User.find_by(id: "#{Lesson.last.user_id}").time_zone
 
 		mail to: user.email, subject: "Clase de #{Language.find_by(id: "#{Lesson.last.language_id}").name} con #{User.find_by(id: "#{Lesson.last.student_id}").name}"
 
@@ -17,9 +18,10 @@ class LessonMailer < ApplicationMailer
 	def student_email(user)
 		@user = user
 		@teacher_name = User.find_by(id: "#{Lesson.last.user_id}").name
-		@lesson_date = Lesson.last.date_id.to_formatted_s(:long)
+		@lesson_date = Lesson.last.date_id.in_time_zone(User.find_by(id: "#{Lesson.last.student_id}").time_zone).to_formatted_s(:long_ordinal)
 		@lesson_duration = Lesson.last.time_duration_id/60
 		@lesson_language = Language.find_by(id: "#{Lesson.last.language_id}").name
+		@student_timezone = User.find_by(id: "#{Lesson.last.student_id}").time_zone
 		
 
 		mail to: User.find_by(id: "#{Lesson.last.student_id}").email, subject: "Clase de #{Language.find_by(id: "#{Lesson.last.language_id}").name} con #{User.find_by(id: "#{Lesson.last.user_id}").name}"

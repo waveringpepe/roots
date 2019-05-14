11b3f688fc34665adcf68cb225842431588f3811
Jose Degollado <josedegollado@MacBook-Air-de-Jose.local>
2019-05-13 21:14:33 -0500

    fixed after sign up confirm page for teachers and students, and set up a message for students when they do not have a teacher assigned

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -2,6 +2,10 @@ class ConfirmationsController < Devise::ConfirmationsController
   private
   def after_confirmation_path_for(resource_name, resource)
     sign_in(resource) # In case you want to sign in the user
-    memberships_path
+    if logged_in?(:teacher)
+    	lessons_path
+    elsif logged_in?(:user)
+    	memberships_path
+    end
   end
 end

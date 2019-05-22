47937b43459f564ee4c831c0ed0c29ab8bbec012
Jose Degollado <josedegollado@MacBook-Air-de-Jose.local>
2019-05-22 17:45:20 -0500

    added sum of hours of class in teachers profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,8 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   access user: {except: [:index]}, teacher: {except: [:index]}, admin: :all
 
-
-
   # GET /User
   def index
-    @users = User.all
     @users = User.paginate(:page => params[:page], :per_page => 100).order('created_at DESC')
     @renderer = custom_paginate_renderer
     @lessons = Lesson.all

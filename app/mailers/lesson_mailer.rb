acbdec4a64f8c49a66738855fb01e9d0be78b7c9
Jose Degollado <josedegollado@MacBook-Air-de-Jose.local>
2019-05-21 19:04:13 -0500

    fixed grammar in mailers

--- app/mailers/lesson_mailer.rb
+++ app/mailers/lesson_mailer.rb
@@ -5,9 +5,9 @@ class LessonMailer < ApplicationMailer
 
 	def teacher_email(lesson)
 		@lesson = lesson
-		@teacher_name = User.find_by(id: "#{@lesson.user_id}").name
+		@teacher_name = User.find_by(id: "#{@lesson.user_id}").name.capitalize
 		@teacher_timezone = User.find_by(id: "#{@lesson.user_id}").time_zone
-		@student_name = User.find_by(id: "#{@lesson.student_id}").name
+		@student_name = User.find_by(id: "#{@lesson.student_id}").name.capitalize
 		@student_timezone = User.find_by(id: "#{@lesson.student_id}").time_zone
 		@lesson_date = @lesson.date_id.in_time_zone(@teacher_timezone).to_formatted_s(:long_ordinal)
 		@lesson_duration = @lesson.time_duration_id/60
@@ -16,15 +16,15 @@ class LessonMailer < ApplicationMailer
 		@student_email = User.find_by(id: "#{@lesson.student_id}").email
 
 
-		mail to: @teacher_email , subject: "#{t('mailer_subject_1')} #{@lesson_language} #{t('mailer_subject_1_a')} #{@student_name}"
+		mail to: @teacher_email , subject: "#{t('mail_lessons_lesson_of')} #{@lesson_language} #{t('mailer_subject_1_a')} #{@student_name}"
 
 	end
 
 	def student_email(lesson)
 		@lesson = lesson
-		@teacher_name = User.find_by(id: "#{@lesson.user_id}").name
+		@teacher_name = User.find_by(id: "#{@lesson.user_id}").name.capitalize
 		@teacher_timezone = User.find_by(id: "#{@lesson.user_id}").time_zone
-		@student_name = User.find_by(id: "#{@lesson.student_id}").name
+		@student_name = User.find_by(id: "#{@lesson.student_id}").name.capitalize
 		@student_timezone = User.find_by(id: "#{@lesson.student_id}").time_zone
 		@lesson_date = @lesson.date_id.in_time_zone(@student_timezone).to_formatted_s(:long_ordinal)
 		@lesson_duration = @lesson.time_duration_id/60
@@ -33,40 +33,40 @@ class LessonMailer < ApplicationMailer
 		@student_email = User.find_by(id: "#{@lesson.student_id}").email
 		
 
-		mail to: @student_email, subject: "#{t('mailer_subject_1')} #{@lesson_language} #{t('mailer_subject_1_a')} #{@teacher_name}"
+		mail to: @student_email, subject: "#{t('mail_lessons_lesson_of')} #{@lesson_language} #{t('mailer_subject_1_a')} #{@teacher_name}"
 
 	end
 
 	def cancel_lesson_student(lesson)
 		@lesson = lesson
-		@teacher_name = User.find_by(id: "#{@lesson.user_id}").name
-		@student_name = User.find_by(id: "#{@lesson.student_id}").name
+		@teacher_name = User.find_by(id: "#{@lesson.user_id}").name.capitalize
+		@student_name = User.find_by(id: "#{@lesson.student_id}").name.capitalize
 		@teacher_email = User.find_by(id: "#{@lesson.user_id}").email
 		@lesson_language = Language.find_by(id: "#{@lesson.language_id}").name
 		@teacher_timezone = User.find_by(id: "#{@lesson.user_id}").time_zone
 		@lesson_date = @lesson.date_id.in_time_zone(@teacher_timezone).to_formatted_s(:long_ordinal)
 
-		mail to: @teacher_email, subject: "#{User.find_by(id: "#{@lesson.student_id}").name} #{t('mailer_subject_2')} #{Language.find_by(id: "#{@lesson.language_id}").name}"
+		mail to: @teacher_email, subject: "#{@student_name} #{t('mailer_subject_2')} #{@lesson_language}"
 
 	end
 
 	def cancel_lesson_teacher(lesson)
 		@lesson = lesson
-		@teacher_name = User.find_by(id: "#{@lesson.user_id}").name
-		@student_name = User.find_by(id: "#{@lesson.student_id}").name
+		@teacher_name = User.find_by(id: "#{@lesson.user_id}").name.capitalize
+		@student_name = User.find_by(id: "#{@lesson.student_id}").name.capitalize
 		@student_email = User.find_by(id: "#{@lesson.student_id}").email
 		@lesson_language = Language.find_by(id: "#{@lesson.language_id}").name
 		@student_timezone = User.find_by(id: "#{@lesson.student_id}").time_zone
 		@lesson_date = @lesson.date_id.in_time_zone(@teacher_timezone).to_formatted_s(:long_ordinal)
 
-		mail to: @student_email, subject: "#{User.find_by(id: "#{@lesson.user_id}").name} #{t('mailer_subject_2')} #{Language.find_by(id: "#{@lesson.language_id}").name}"
+		mail to: @student_email, subject: "#{@teacher_name} #{t('mailer_subject_2')} #{@lesson_language}"
 
 	end
 
 	def reminder_lesson_student_1(lesson)
 		@lesson = lesson
-		@teacher_name = User.find_by(id: "#{@lesson.user_id}").name
-		@student_name = User.find_by(id: "#{@lesson.student_id}").name
+		@teacher_name = User.find_by(id: "#{@lesson.user_id}").name.capitalize
+		@student_name = User.find_by(id: "#{@lesson.student_id}").name.capitalize
 		@student_email = User.find_by(id: "#{@lesson.student_id}").email
 		@lesson_language = Language.find_by(id: "#{@lesson.language_id}").name
 		@student_timezone = User.find_by(id: "#{@lesson.student_id}").time_zone
@@ -78,10 +78,10 @@ class LessonMailer < ApplicationMailer
 
 	def reminder_lesson_teacher_1(lesson)
 		@lesson = lesson
-		@teacher_name = User.find_by(id: "#{@lesson.user_id}").name
+		@teacher_name = User.find_by(id: "#{@lesson.user_id}").name.capitalize
 		@teacher_email = User.find_by(id: "#{@lesson.user_id}").email
 		@teacher_timezone = User.find_by(id: "#{@lesson.user_id}").time_zone
-		@student_name = User.find_by(id: "#{@lesson.student_id}").name
+		@student_name = User.find_by(id: "#{@lesson.student_id}").name.capitalize
 		@student_email = User.find_by(id: "#{@lesson.student_id}").email
 		@lesson_language = Language.find_by(id: "#{@lesson.language_id}").name
 		@student_timezone = User.find_by(id: "#{@lesson.student_id}").time_zone
@@ -93,8 +93,8 @@ class LessonMailer < ApplicationMailer
 
 	def reminder_lesson_student_2(lesson)
 		@lesson = lesson
-		@teacher_name = User.find_by(id: "#{@lesson.user_id}").name
-		@student_name = User.find_by(id: "#{@lesson.student_id}").name
+		@teacher_name = User.find_by(id: "#{@lesson.user_id}").name.capitalize
+		@student_name = User.find_by(id: "#{@lesson.student_id}").name.capitalize
 		@student_email = User.find_by(id: "#{@lesson.student_id}").email
 		@lesson_language = Language.find_by(id: "#{@lesson.language_id}").name
 		@student_timezone = User.find_by(id: "#{@lesson.student_id}").time_zone
@@ -106,10 +106,10 @@ class LessonMailer < ApplicationMailer
 
 	def reminder_lesson_teacher_2(lesson)
 		@lesson = lesson
-		@teacher_name = User.find_by(id: "#{@lesson.user_id}").name
+		@teacher_name = User.find_by(id: "#{@lesson.user_id}").name.capitalize
 		@teacher_email = User.find_by(id: "#{@lesson.user_id}").email
 		@teacher_timezone = User.find_by(id: "#{@lesson.user_id}").time_zone
-		@student_name = User.find_by(id: "#{@lesson.student_id}").name
+		@student_name = User.find_by(id: "#{@lesson.student_id}").name.capitalize
 		@student_email = User.find_by(id: "#{@lesson.student_id}").email
 		@lesson_language = Language.find_by(id: "#{@lesson.language_id}").name
 		@student_timezone = User.find_by(id: "#{@lesson.student_id}").time_zone

de9100850ef9ad6b3ce0b82630adc65880666bc1
Jose Degollado <josedegollado@MacBook-Air-de-Jose-3.local>
2019-06-25 17:44:15 -0500

    deleted template for index users and placed all in index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,6 @@ class UsersController < ApplicationController
   # GET /User
   def index
     @users = User.paginate(:page => params[:page], :per_page => 100).order(sort_column + " " + sort_direction).where("name LIKE ?", "%#{params[:search]}%")
-    @renderer = custom_paginate_renderer
-    @lessons = Lesson.all
-
   end
  
   # GET /User

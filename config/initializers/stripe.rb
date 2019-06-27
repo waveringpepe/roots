a38e2cefe5adc3054b34b509681e1928044155a7
Jose Degollado <josedegollado@MacBook-Air-de-Jose-3.local>
2019-06-26 20:16:35 -0500

    changed file

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,9 +1,11 @@
 Stripe.api_key = ENV["stripe_api_key"]
-STRIPE_PUBLIC_KEY = ENV["stripe_publishable_key"]
-StripeEvent.signing_secret = ENV["stripe_webhooks_key"]
+StripeEvent.signing_secret = ENV['STRIPE_SIGNING_SECRET']
+
+
+
 
 
 StripeEvent.configure do |events|
-	events.subscribe "charge.succeeded", Webhooks::ChargeSucceeded.new
+	
 	events.subscribe "invoice.payment_succeeded", Webhooks::PaymentSucceeded.new
 end

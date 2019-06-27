4ddc1e6a6b8572b810044dda76817c87645806c2
Jose Degollado <josedegollado@MacBook-Air-de-Jose-3.local>
2019-06-26 19:25:40 -0500

    added key signinto stripe event_gem

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,7 @@
 Stripe.api_key = ENV["stripe_api_key"]
 STRIPE_PUBLIC_KEY = ENV["stripe_publishable_key"]
+StripeEvent.signing_secret = ENV["stripe_webhooks_key"]
+
 
 StripeEvent.configure do |events|
 	events.subscribe "charge.succeeded", Webhooks::ChargeSucceeded.new
